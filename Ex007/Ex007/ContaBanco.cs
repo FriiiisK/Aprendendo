2603ffb9bd614324551fc61208955b25db5da194
FriiiisK <victor_emanuel_ribas@hotmail.com>
2023-06-05 20:51:00 -0300

    Aprendendo C#

---
+++ Ex007/Ex007/ContaBanco.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Ex007
+{
+     class ContaBanco
+    {
+        public string Nome;
+        public int Id;
+        public double ValorInicial;
+
+
+
+        public ContaBanco(int id, String nome, Double valorinicial)
+        {
+            Nome = nome;
+            Id = id;
+            ValorInicial = valorinicial;
+        }
+
+        public void Deposito(double deposito)
+        {
+            ValorInicial = ValorInicial + deposito;
+        }
+        public void Saque(double saque)
+        {
+            ValorInicial = ValorInicial - saque - 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + Id + ", Titular: " + Nome + ", Saldo: $ " + ValorInicial.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+
+
+
+    }
+}
+
+
+
+
+
---
+++ Ex007/Ex007/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+namespace Ex007
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o número da conta bancária: ");
+            int Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre o titular da conta: ");
+            String Nome = Console.ReadLine();
+            Console.WriteLine("Haverá um deposito inicial? (s/n)");
+            char DepositoInicial = char.Parse(Console.ReadLine());
+            Double ValorInicial = 0;
+
+
+
+            if (DepositoInicial == 's' || DepositoInicial == 'S')
+            {
+                Console.WriteLine("Insira o valor de deposito inicial: ");
+                ValorInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+             
+            }
+            
+            ContaBanco conta = new ContaBanco(Id, Nome, ValorInicial);
+            Console.WriteLine(conta);
+            Console.Write("Insira um valor para deposito: ");
+            conta.Deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine(conta);
+            Console.Write("Insira um valor para saque: ");
+            conta.Saque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+            
+        }
+    }
+}
